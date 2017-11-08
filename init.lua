5e405665474c489c5092932550638a99c7037eb2
Miab0t <nycexs@gmail.com>
2017-11-08 16:21:46 -0500

    handlers/

--- init.lua
+++ init.lua
@@ -1,21 +1,9 @@
-﻿-- Arcade Mod by miab0t
--- Special thanks to Karmatized for help with the ramdomization
--- This package started as addendem to the Home Decor Mod written by VanessaEcode 
--- Sound files by: 
--- cmusounddesign https://www.freesound.org/people/cmusounddesign/sounds/84710/ cropped for the game machines
+local handlerpath = arcade.modpath .. "/handlers/"
+
+dofile(handlerpath.."nodeboxes.lua")
+
+dofile(handlerpath.."inventory.lua")
+
+dofile(handlerpath.."registration.lua")
 
 
-
-local modpath = minetest.get_modpath("arcade")
-
-arcade = {
-	modpath = modpath,
-
-	gettext = rawget(_G, "intllib") and intllib.Getter() or function(s) return s end,
-
-	expect_infinite_stacks = minetest.setting_getbool("creative_mode") and not minetest.get_modpath("unified_inventory")
-}
-dofile(modpath.."/coingame.lua")
-dofile(modpath.."/clawgame.lua")
-dofile(modpath.."/handlers/init.lua")
-print("[arcade] " .. moremachines.gettext("Loaded!"))
